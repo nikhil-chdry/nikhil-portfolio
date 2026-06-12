53427ed92f9d4a0695086f8827351c873075faa1
Nikhil <nikhilsunda42@gmail.com>
2026-06-12 16:15:46 +0530

    updated the api path.js

--- api/[[...path]].js
+++ api/[[...path]].js
@@ -1,7 +1,29 @@
 import handler from "../dist/server/server.js";
 
-export const config = { runtime: "edge" };
+export const config = { runtime: "nodejs" };
 
-export default async function (request) {
-  return handler.fetch(request, {}, {});
+export default async function (req, res) {
+  // Convert Node req to a Fetch Request
+  const url = `https://${req.headers.host}${req.url}`;
+  const request = new Request(url, {
+    method: req.method,
+    headers: req.headers,
+    body: ["GET", "HEAD"].includes(req.method) ? undefined : req,
+    duplex: "half",
+  });
+
+  const response = await handler.fetch(request, {}, {});
+
+  res.statusCode = response.status;
+  response.headers.forEach((value, key) => res.setHeader(key, value));
+
+  if (response.body) {
+    const reader = response.body.getReader();
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+      res.write(value);
+    }
+  }
+  res.end();
 }
